8301cad6ee944a77a1585070b593984d4c0ff78f
Bhanvidewan <69968160+Bhanvidewan@users.noreply.github.com>
2020-11-12 22:14:45 +0530

    Update q1.java

--- Assignment5/q1.java
+++ Assignment5/q1.java
@@ -10,21 +10,21 @@ package solution;
  * @author bhanvidewan
  */
 public class Q1{
-public static void main(String... args) {
-        Thread thread = new Thread() {
-            public void run() {
-                int x = 0;
-                while (x<101) {
-                    x = x + 1;
-                    System.out.print(x + " ");
-                    if (x % 10 == 0)
+public static void main(String... args){
+        Thread thread=new Thread(){
+            public void run(){
+                int x=0;
+                while(x<101){
+                    x++;
+                    System.out.print(x+" ");
+                    if((x%10)==0)
                     {   System.out.println();
                         System.out.println("10 second message");
-                        
+                        System.out.println();
                     }
-                    try {
+                    try{
                         Thread.sleep(1000);
-                    } catch (Exception e) {
+                    } catch (Exception e){
                         e.printStackTrace();
                     }
                 }
